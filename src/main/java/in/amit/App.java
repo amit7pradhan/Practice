2d5f85fca2423a517ec64a08b5f9e6e623b28c85
Amit <amit7pradhan@gmail.com>
2024-03-24 11:01:14 +0530

    initial

---
+++ src/main/java/in/amit/App.java
@@ -0,0 +1,16 @@
+package in.amit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        ini a =10;
+        String msg = "i changed";
+        int b = 20;
+        System.out.println( "Hello World!" );
+    }
+}
